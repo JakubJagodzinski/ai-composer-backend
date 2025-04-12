cdaae5e3b268c73d0544e00d4d4bd2dabe18b068
JagodaJakub <kubajagjag@gmail.com>
2025-04-12 20:06:08 +0200

    Added handlers to send response based on file_type received from client in request

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
 import io
 
 from flask import Flask, send_file, request, render_template
-from pydub import AudioSegment
+
+from file_type import FileType
+from files_utils import load_midi_from_disk, midi_to_bytes, midi_to_mp3
 
 app = Flask(__name__)
 
 
-def generate_music_with_neural_network(args):
-    filename = r"C:\Users\kubaj\Desktop\music.wav"
-    audio = AudioSegment.from_wav(filename)
-    mp3_file = io.BytesIO()
-    audio.export(mp3_file, format="mp3")
-    mp3_file.seek(0)
-    return mp3_file
-
-
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
 @app.route('/generate_music', methods=['POST'])
@@ -28,11 +21,20 @@ def generate_music():
     seconds = data.get('seconds')
     file_type = data.get('file_type')
 
-    args = (music_genre, main_instrument, seconds, file_type)
+    network_args = (music_genre, main_instrument, seconds)
 
-    mp3_file = generate_music_with_neural_network(args)
+    midi_file = load_midi_from_disk()  # Mock, replace with neural network call
+    midi_bytes = midi_to_bytes(midi_file)
 
-    return send_file(mp3_file, mimetype='audio/mpeg', as_attachment=True, download_name='music.mp3')
+    if file_type == FileType.MP3:
+        mp3_bytes = midi_to_mp3(midi_bytes, 'FluidR3_GM.sf2')
+        mp3_buffer = io.BytesIO(mp3_bytes)
+        mp3_buffer.seek(0)
+        return send_file(mp3_buffer, mimetype='audio/mpeg', as_attachment=True, download_name='music.mp3')
+    elif file_type == FileType.MIDI:
+        midi_buffer = io.BytesIO(midi_bytes)
+        midi_buffer.seek(0)
+        return send_file(midi_buffer, mimetype='audio/midi', as_attachment=True, download_name='music.mid')
 
 
 if __name__ == '__main__':
