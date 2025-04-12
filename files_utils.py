2876cf7da15efda570fae2c3f111c43edf3f6a24
JagodaJakub <kubajagjag@gmail.com>
2025-04-12 20:05:16 +0200

    Created function to convert midi to mp3 and function to convert midi to bytes

---
+++ files_utils.py
@@ -0,0 +1,43 @@
+import io
+import os
+
+from midi2audio import FluidSynth
+from mido import MidiFile
+from pydub import AudioSegment
+
+
+def midi_to_bytes(midi: MidiFile) -> bytes:
+    """Convert a MidiFile object to bytes."""
+    midi_buffer = io.BytesIO()
+    midi.save(file=midi_buffer)
+    midi_buffer.seek(0)
+    return midi_buffer.getvalue()
+
+
+def midi_to_mp3(midi_bytes: bytes, soundfont_path: str) -> bytes:
+    """Convert MIDI bytes to MP3 bytes using FluidSynth and pydub."""
+
+    # Save MIDI to temporary file
+    with open('temp.mid', 'wb') as midi_file:
+        midi_file.write(midi_bytes)
+
+    # Convert MIDI to WAV using FluidSynth
+    fs = FluidSynth(soundfont_path)
+    fs.midi_to_audio('temp.mid', 'temp.wav')
+
+    # Convert WAV to MP3 using pydub
+    audio = AudioSegment.from_wav('temp.wav')
+    mp3_io = io.BytesIO()
+    audio.export(mp3_io, format='mp3')
+    mp3_io.seek(0)
+
+    # Remove temporary files
+    os.remove('temp.mid')
+    os.remove('temp.wav')
+
+    return mp3_io.getvalue()
+
+
+def load_midi_from_disk():
+    """Mock function to load a MIDI file from disk."""
+    return MidiFile('sample.mid')
