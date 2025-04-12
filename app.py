2ca9a58c784e7e18cfc7885f6c55c6c2736fef71
JagodaJakub <kubajagjag@gmail.com>
2025-04-12 20:45:25 +0200

    Added welcome message to home endpoint

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
+    return 'Welcome to AI Composer!'
 
 @app.route('/generate_music', methods=['POST'])
 def generate_music():
